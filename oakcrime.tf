d85373b2d1586f7b2b32e8ad9e21de5d00773245
Aaron D Borden <adborden@a14n.net>
2019-02-18 17:10:41 -0800

    Split teams into separate files

---
+++ oakcrime.tf
@@ -0,0 +1,19 @@
+variable "oakcrime_prod_db_password" {
+  description = "db_password for the production app."
+}
+
+variable "oakcrime_prod_django_secret_key" {
+  description = "django_secret_key for the production app."
+}
+
+module "oakcrime" {
+  source = "./modules/oakcrime"
+  security_group_name = "${aws_security_group.ssh_and_web.name}"
+  key_pair_id = "${aws_key_pair.openoakland.id}"
+  zone_id = "${data.aws_route53_zone.openoakland.id}"
+
+  # Beanstalk apps
+  prod_db_password = "${var.oakcrime_prod_db_password}"
+  prod_django_secret_key = "${var.oakcrime_prod_django_secret_key}"
+}
+
